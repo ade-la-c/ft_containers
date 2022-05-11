be5190791cddca19f627920ca72832e847d14274
ade-la-c <gringodlc@gmail.com>
2022-05-11 17:17:48 +0200

    update

---
+++ iterator/Iterator.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Iterator.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-la-c <ade-la-c@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/09 19:04:38 by ade-la-c          #+#    #+#             */
+/*   Updated: 2022/05/11 13:52:36 by ade-la-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ITERATOR_H
+# define ITERATOR_H
+
+# include <iostream>
+# include <memory>
+
+namespace ft {
+
+
+	template< class Category, class T, class Distance = ptrdiff_t, class Pointer = T*, class Reference = T& >
+	class	iterator {
+
+
+	public:
+
+		
+
+
+	private:
+
+
+
+	};
+
+
+}
+
+#endif
