98f6fe5fa5727e197b63f1a5e80ae7cf5c46f6f0
ade-la-c <gringodlc@gmail.com>
2022-05-13 18:22:22 +0200

    oe

--- vector/Vector.hpp
+++ vector/Vector.hpp
@@ -6,7 +6,7 @@
 /*   By: ade-la-c <ade-la-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 18:35:09 by ade-la-c          #+#    #+#             */
-/*   Updated: 2022/05/11 14:39:38 by ade-la-c         ###   ########.fr       */
+/*   Updated: 2022/05/13 18:21:31 by ade-la-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,18 +27,18 @@ namespace	ft {
 
 		//	Type definers
 
-		typedef T value_type;
-		typedef Alloc allocator_type;
-		typedef typename allocator_type::reference reference;
-		typedef typename allocator_type::const_reference const_reference;
-		typedef typename allocator_type::pointer pointer;
-		typedef typename allocator_type::const_pointer const_pointer;
-		typedef typename ft::wrap_iter<pointer> iterator;
-		typedef typename ft::wrap_iter<const_pointer> const_iterator;
-		typedef typename ft::reverse_iterator<iterator> reverse_iterator;
-		typedef typename ft::reverse_iterator<const_iterator> const_reverse_iterator;
-		typedef typename allocator_type::size_type size_type;
-		typedef typename allocator_type::difference_type difference_type;
+		typedef T													value_type;
+		typedef Alloc												allocator_type;
+		typedef typename allocator_type::reference					reference;
+		typedef typename allocator_type::const_reference			const_reference;
+		typedef typename allocator_type::pointer					pointer;
+		typedef typename allocator_type::const_pointer				const_pointer;
+		typedef typename ft::wrap_iter<pointer>						iterator;
+		typedef typename ft::wrap_iter<const_pointer>				const_iterator;
+		typedef typename ft::reverse_iterator<iterator>				reverse_iterator;
+		typedef typename ft::reverse_iterator<const_iterator>		const_reverse_iterator;
+		typedef typename allocator_type::size_type					size_type;
+		typedef typename allocator_type::difference_type			difference_type;
 
 		//	MEMBER FUNCTIONS	//
 
