d4a6b090ca191d16abed8a7194d0b036277ad6dc
ade-la-c <gringodlc@gmail.com>
2022-09-20 18:46:11 +0200

    writing map prototypes

--- Containers/Map.hpp
+++ Containers/Map.hpp
@@ -47,24 +47,59 @@ namespace   ft {
 		typedef typename allocator_type::pointer								pointer;
 		typedef typename allocator_type::const_pointer							const_pointer;
 
-		typedef ft::reverse_iterator< value_type >						reverse_iterator;		 //???????
-		typedef ft::reverse_iterator< const value_type >				const_reverse_iterator; //???????
-		typedef typename std::bidirectional_iterator< value_type >				bidirectional_iterator;
-		typedef typename std::bidirectional_iterator< const value_type >		const_bidirectional_iterator;
+		typedef ft::reverse_iterator< value_type >								reverse_iterator;		 //???????
+		typedef ft::reverse_iterator< const value_type >						const_reverse_iterator; //???????
+		typedef typename std::bidirectional_iterator< value_type >				iterator;
+		typedef typename std::bidirectional_iterator< const value_type >		const_iterator;
 
-		//	methods
+		//	methods		//
 
-		map( void );
+		//*	empty
 		explicit	map( const key_compare & comp = key_compare(), const allocator_type & alloc = allocator_type() );
+		//*	range
 		template <class InputIterator>
 		map( InputIterator first, InputIterator last, const key_compare & comp = key_compare(), const allocator_type & alloc = allocator_type() );
+		//*	copy
 		map( const map & x );
+		//*	assignation operator
+		map &		operator=( const map & x );
+		//*	destructor
+		~map( void );
+
+		// iterators
+
+		iterator				begin( void );
+		const_iterator			begin( void ) const;
+		iterator				end( void );
+		const_iterator			end( void ) const;
+		reverse_iterator		rbegin( void );
+		const_reverse_iterator	rbegin( void ) const;
+		reverse_iterator		rend( void );
+		const_reverse_iterator	rend( void ) const;
+
+		// size
+
+		bool			empty( void ) const { return !_size; }	//! allowed ?
+		size_type		size( void ) const { return _size; }
+		size_type		max_size( void ) const { return std::numeric_limits<size_type>::max() / sizeof(value_type); }
+
+		// element access
+
+		mapped_type &	operator[]( const key_type & k );
+
+		// modifiers
+
+		//*	single element
+		pair<iterator, bool>	insert( const value_type & val );
+		iterator				insert( iterator position, value_type & val );
+		template<class InputIterator>
+		void					insert( InputIterator first, InputIterator last );
 
 
 
 	private:
 
-
+		size_type		_size;
 
 
 	}
